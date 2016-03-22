9f4b90f7cb6f407f15b49d49b63652e4d165e91d
beHappyNow <artemb.step@gmail.com>
2016-03-22 04:09:31 +0200

    1. Migration for user table 2. User model and controller added 3. fb library added

---
+++ testAPI/api/models/User.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%user}}".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $image
+ * @property string $lat
+ * @property string $lon
+ * @property string $city
+ * @property string $country
+ * @property string $gender
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%user}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'auth_key', 'password_hash', 'email', 'created_at', 'updated_at'], 'required'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['username', 'password_hash', 'password_reset_token', 'email', 'first_name', 'last_name', 'image', 'lat', 'lon', 'city', 'country', 'gender'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
+            [['password_reset_token'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'auth_key' => 'Auth Key',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'email' => 'Email',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'image' => 'Image',
+            'lat' => 'Lat',
+            'lon' => 'Lon',
+            'city' => 'City',
+            'country' => 'Country',
+            'gender' => 'Gender',
+        ];
+    }
+}
